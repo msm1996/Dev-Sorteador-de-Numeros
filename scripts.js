26f4c7048a8a8a7926c970518c5af25d44786cde
msm1996 <master19041996@gmail.com>
2025-09-08 22:45:10 -0300

    commitar Sorteador de Numeros

--- scripts.js
+++ scripts.js
@@ -2,9 +2,16 @@ function gerenciarNumero(){
     const min =  Math.ceil (document.querySelector(".min").value)
     const max = Math.floor (document.querySelector(".max").value)
 
-    const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min < max){
+        const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(resultado)
+    }else{
+        alert("o valor minimo tem que ser menor que o maior")
+    }
 
-    alert(resultado)
+    
+
+    
 
     
 
